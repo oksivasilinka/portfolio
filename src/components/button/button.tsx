a1c6c4e4a1678d5ad16560c198c734ad6130cb19
Oksana <oksana.s.kovalchuk@gmail.com>
2023-11-13 12:35:34 +0300

    fix styles main

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,3 +1,23 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { theme } from 'styles/theme'
 
-export const Button = styled.button``
+type Props = {
+  variant?: 'primary' | 'outline'
+}
+
+export const Button = styled.button<Props>`
+  background-color: ${theme.colors.accent};
+  color: ${theme.colors.primaryBg};
+  padding: 10px 20px;
+  border-radius: 24px;
+  font-size: 18px;
+  font-weight: 700;
+
+  ${(props) =>
+    props.variant === 'outline' &&
+    css<Props>`
+      background-color: transparent;
+      color: ${theme.colors.accent};
+      border: 1px solid ${theme.colors.accent};
+    `}
+`
--- src/layout/sections/main/main.tsx
+++ src/layout/sections/main/main.tsx
@@ -2,21 +2,25 @@ import photo from 'assets/SAVE_20200927_223544.webp'
 import styled from 'styled-components'
 import { FlexWrapper } from 'components/flex-wrapper'
 import { Button } from 'components/button/button'
+import { theme } from 'styles/theme'
+import { Container } from 'components/container/container'
 
 export const Main = () => {
   return (
     <StyledMain>
-      <FlexWrapper gap={'50px'} align={'center'} justify={'center'}>
-        <Photo src={photo} alt={'main-photo'} />
-        <FlexWrapper direction={'column'} gap={'30px'}>
-          <Name>Oksana Kovalchuk</Name>
-          <MainTitle>Frontend Developer, Minsk</MainTitle>
-          <FlexWrapper gap={'20px'}>
-            <Button>Send Message</Button>
-            <Button>Download CV</Button>
+      <Container>
+        <FlexWrapper justify={'center'} gap={'50px'} align={'center'}>
+          <Photo src={photo} alt={'main-photo'} />
+          <FlexWrapper direction={'column'} gap={'30px'}>
+            <Name>Oksana Kovalchuk</Name>
+            <MainTitle>Frontend Developer, Minsk</MainTitle>
+            <FlexWrapper gap={'10px'}>
+              <Button>Send Message</Button>
+              <Button variant={'outline'}>Download CV</Button>
+            </FlexWrapper>
           </FlexWrapper>
         </FlexWrapper>
-      </FlexWrapper>
+      </Container>
     </StyledMain>
   )
 }
@@ -26,13 +30,22 @@ const StyledMain = styled.section`
   align-items: center;
   justify-content: center;
   height: 100vh;
-  background-color: cornsilk;
+  padding: 150px;
 `
 
 const Photo = styled.img`
-  width: 310px;
+  width: 500px;
   object-fit: cover;
+  border-radius: 24px;
 `
 
-const MainTitle = styled.h1``
-const Name = styled.h2``
+const Name = styled.h2`
+  color: ${theme.colors.accent};
+  font-size: 48px;
+  font-weight: 800;
+`
+
+const MainTitle = styled.h1`
+  font-size: 20px;
+  font-weight: 700;
+`
