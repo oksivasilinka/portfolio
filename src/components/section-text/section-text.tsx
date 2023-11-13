4dbc0e308837a49e1233759b5f9d56aeed914ac2
Oksana <oksana.s.kovalchuk@gmail.com>
2023-11-13 20:21:36 +0300

    fix styles about me

--- src/components/section-text/section-text.tsx
+++ src/components/section-text/section-text.tsx
@@ -3,4 +3,7 @@ import styled from 'styled-components'
 export const SectionText = styled.span`
   display: block;
   text-align: center;
+  font-size: 18px;
+  font-weight: 400;
+  padding: 50px 200px;
 `
--- src/components/section-title/section-title.tsx
+++ src/components/section-title/section-title.tsx
@@ -1,7 +1,13 @@
 import styled from 'styled-components'
+import { theme } from 'styles/theme'
 
 export const SectionTitle = styled.h2`
   text-align: center;
   padding-top: 80px;
   padding-bottom: 40px;
+  font-size: 34px;
+  font-weight: 700;
+  letter-spacing: 6.8px;
+  color: ${theme.colors.accent};
+  border-bottom: 1px solid ${theme.colors.secondary};
 `
